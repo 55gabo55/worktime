18e34a195ca96386bbba3bb23639d17f4ce43358
Gabriel Kostialik <g.kostialik@gmail.com>
2020-09-12 20:37:37 +0200

    Initial commit.

---
+++ src/main/java/sk/gabrielkostiali/workTime/WorkTimeApplication.java
@@ -0,0 +1,13 @@
+package sk.gabrielkostiali.workTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkTimeApplication.class, args);
+	}
+
+}
